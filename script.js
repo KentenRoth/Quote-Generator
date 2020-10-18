90e7e343ac365bdc77959f9cf8bd41be00851ccb
Kent Roth <kentencroth@yahoo.com>
2020-10-18 10:05:35 -0500

    Connected New quote and tweet button

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const twitterButton = document.getElementById('twitter-button');
+const newQuoteButton = document.getElementById('new-quote');
+
 async function getQuote() {
 	const randomNumber = Math.floor(Math.random() * 1642);
 	const apiURL = 'https://type.fit/api/quotes';
 	try {
 		const response = await fetch(apiURL);
 		const data = await response.json();
-		console.log(data[randomNumber]);
+		const quote = data[randomNumber];
+		if (quote.author.length === 0) {
+			quoteAuthor.innerText = 'Unknown';
+		}
+		if (quote.text.length > 100) {
+			quoteText.classList.add('long-quote');
+		} else {
+			quoteText.classList.remove('long-quote');
+		}
+		quoteText.innerText = quote.text;
+		quoteAuthor.innerText = quote.author;
 	} catch (error) {
 		console.log(error);
 	}
 }
+
+function tweetQuote() {
+	const quote = quoteText.innerText;
+	const author = quoteAuthor.innerText;
+	const twitterURL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+	window.open(twitterURL, '_blank');
+}
+
+newQuoteButton.addEventListener('click', getQuote);
+twitterButton.addEventListener('click', tweetQuote);
 
 getQuote();
