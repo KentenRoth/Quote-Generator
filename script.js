db06d90f78758558d4af012fbbf4b8e7f3740eab
Kent Roth <kentencroth@yahoo.com>
2020-10-19 12:56:53 -0500

    Added spinner when loading

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterButton = document.getElementById('twitter-button');
 const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+	if (!loader.hidden) {
+		quoteContainer.hidden = false;
+		loader.hidden = true;
+	}
+}
 
 async function getQuote() {
+	showLoadingSpinner();
 	const randomNumber = Math.floor(Math.random() * 1642);
 	const apiURL = 'https://type.fit/api/quotes';
 	try {
@@ -21,6 +35,7 @@ async function getQuote() {
 		}
 		quoteText.innerText = quote.text;
 		quoteAuthor.innerText = quote.author;
+		removeLoadingSpinner();
 	} catch (error) {
 		console.log(error);
 	}
